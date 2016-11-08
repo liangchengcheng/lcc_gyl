f4f77472cb1a7148b36e6ff3122459e6981b546c
liangchengcheng <1038127753@qq.com>
2016-11-08 22:44:58 +0800

    add action class file
    add action class file

--- src/main/java/com/lcc/gyl/login/action/LoginAction.java
+++ src/main/java/com/lcc/gyl/login/action/LoginAction.java
@@ -5,12 +5,27 @@ import javax.annotation.Resource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.lcc.gyl.base.action.BaseAction;
+import com.lcc.gyl.domain.basedata.User;
 import com.lcc.gyl.login.service.LoginService;
 
-Controller(name="loginAction")
+
+@Controller("loginAction")
 @Scope("prototype")
-public class LoginAction  {
+public class LoginAction extends BaseAction<User> {
 	@Resource(name="loginService")
 	private LoginService loginService;
+	
+	public String authentication(){
+		User user = this.loginService.authentication(this.getModel().getUsername(), this.getModel().getPassword());
+		
+		if (user == null) {
+			this.addActionError("用户名或者密码的错误");
+			return "login";
+		}else{
+			this.getSession().setAttribute("user", user);;
+			return "index";
+		}
+	}
 	
 }
